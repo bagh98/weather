906d9d2836c37e779902c9efc5776a4e361d8b41
bagh98~ <abonaorz@gmail.com>
2025-01-12 16:26:40 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather/pages/home_page.dart';
+
+import 'providers/weather_provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (context) {
+        return WeatherProvider();
+      },
+      child: WeatherApp()));
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor:
+              Provider.of<WeatherProvider>(context).weatherData == null
+                  ? Colors.blue
+                  : Provider.of<WeatherProvider>(context)
+                      .weatherData!
+                      .getThemeColor(),
+          elevation: 10.0,
+        ),
+        primarySwatch: Provider.of<WeatherProvider>(context).weatherData == null
+            ? Colors.blue
+            : Provider.of<WeatherProvider>(context)
+                .weatherData!
+                .getThemeColor(),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/providers/weather_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherProvider extends ChangeNotifier {
+  WeatherModel? _weatherData;
+
+  String? cityName;
+  set weatherData(WeatherModel? weather) {
+    _weatherData = weather;
+    notifyListeners();
+  }
+
+  WeatherModel? get weatherData => _weatherData;
+}
